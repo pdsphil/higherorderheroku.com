5b94a7c5d80c2d2e543acd97d278823b464ce64c
Ryan Daigle <ryan.daigle@gmail.com>
2013-01-17 21:14:13 -0500

    Add referral links to site

--- lib/helpers/page_helpers.rb
+++ lib/helpers/page_helpers.rb
@@ -1,6 +1,22 @@
 module PageHelpers
 
-  def articles(limit = 10)
-    @articles ||= sitemap.resources.select { |r| r.path.start_with?('articles/') }.sort { |a, b| Date.parse(b.data['date']) <=> Date.parse(a.data['date'])}[0..limit]
+  def articles(limit = 15)
+    @articles ||= date_sort(sitemap.resources.select { |r| r.path.start_with?('articles/') })[0..limit]
+  end
+
+  def links(limit = 15)
+    @links ||= date_sort(data.links)[0..limit]
+  end
+
+  def coalesce(col1, col2, limit = 15)
+    date_sort(col1 + col2)[0..limit]
+  end
+
+  def date_sort(col)
+    col.sort { |a, b| Date.parse(b.data['date']) <=> Date.parse(a.data['date'])}
+  end
+
+  def link_entry?(article_or_link)
+    article_or_link.data.key?('url')
   end
 end
